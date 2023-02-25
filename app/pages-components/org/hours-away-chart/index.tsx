8f8c62c26f47e0a21259af108970cea37e334cfc
Isak <contact@granqvist.dev>
2023-02-25 23:32:18 +0100

    Chart color stuff

--- app/pages-components/org/hours-away-chart/index.tsx
+++ app/pages-components/org/hours-away-chart/index.tsx
@@ -9,27 +9,27 @@ const data = {
     {
       label: 'Monday',
       data: [0, 1, 2, 3, 4, 5, 6],
-      backgroundColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgb(0, 25, 60)',
     },
     {
       label: 'Tuesday',
       data: [0, 1, 2, 3, 4, 5, 6],
-      backgroundColor: 'rgb(75, 192, 192)',
+      backgroundColor: 'rgb(123, 146, 178)',
     },
     {
       label: 'Wednesday',
       data: [0, 1, 2, 3, 4, 5, 6],
-      backgroundColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgb(103, 203, 160)',
     },
     {
       label: 'Thursday',
       data: [0, 1, 2, 3, 4, 5, 6],
-      backgroundColor: 'rgb(75, 192, 192)',
+      backgroundColor: 'rgb(24, 26, 42)',
     },
     {
       label: 'Friday',
       data: [0, 1, 2, 3, 4, 5, 6],
-      backgroundColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgb(75, 107, 251)',
     },
   ],
 };
